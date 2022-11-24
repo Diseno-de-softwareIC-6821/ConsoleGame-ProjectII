8893677cf6c86930bce3a7038655505003a2ea36
Esteban <estebanlm@estudiantec.cr>
2022-11-24 01:16:05 -0600

    Proxy medio implementado

--- Socket Server/src/main/java/Functional/Proxy.java
+++ Socket Server/src/main/java/Functional/Proxy.java
@@ -1,12 +1,30 @@
 package Functional;
 
+import Classes.Abstract.Command;
 import Classes.GameObjects.GameCharacter;
+import Classes.ServerClasses.Player;
 import Data.Log;
+import Interfaces.iCommand;
 
-public class Proxy {
+public class Proxy implements iCommand {
 
-    public void example(){
-        Log.getInstance().startLog("test");
-        //game attack
+
+    public Proxy() throws Exception {
+        CommandManger.getInstance().setBasicsCommands();
+        Log.getInstance().startLog("log.txt");
+    }
+
+    @Override
+    public int execute(String[] args, Player player) {
+        System.out.println("Command to execute: " + args.toString());
+        String command = args[0];
+        Command command1 = CommandManger.getInstance().getCommand(command);
+        command1.execute(args, player);
+        try {
+            Log.getInstance().print("Command executed: " + args.toString());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 }
