7f1c4fc6d88d9983359dcbcfa407da5a41c34fc2
Corne1401 <47864642+danielcornejo14@users.noreply.github.com>
2022-11-24 00:14:08 -0600

    small refactors

--- Socket Server/src/main/java/Classes/Commands/SetPlayerCharacteristics.java
+++ Socket Server/src/main/java/Classes/Commands/SetPlayerCharacteristics.java
@@ -4,6 +4,7 @@ import Classes.Abstract.Command;
 import Classes.ServerClasses.Player;
 import Classes.ServerClasses.Server;
 import Interfaces.iCommand;
+import org.json.JSONArray;
 
 import java.io.IOException;
 
@@ -20,25 +21,20 @@ public class SetPlayerCharacteristics extends Command {
 
         //args[0] = nuevo nombre del jugador;
 
-        //args[1] = personaje 1;
-        //args[2 hasta 6] = armas del personaje 1;
-        //args[7] = personaje 2;
-        //args[8 hasta 13] = armas del personaje 2;
-        //args[14] = personaje 3;
-        //args[15 hasta 20] = armas del personaje 3;
-        //args[21] = personaje 4;
-        //args[22 hasta 27] = armas del personaje 4;
+        //args[1] = json con los personajes y sus armas;
 
-        //args[length-1] = id del jugador;
+
+        JSONArray characters = new JSONArray(args[1]);
+
 
         if(this.server.getPlayerByName(args[0]) != null){
             try {
                 // le envia al cliente un 0 porque ya existe un jugador con ese nombre
                 // y cierra la conexion
                 this.player.update("0");
-                this.player.getSocket().close();
+
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                System.out.println("Name already exists; server error");
             }
             return 0;
         }
@@ -46,6 +42,11 @@ public class SetPlayerCharacteristics extends Command {
         this.player.setPlayerName(args[0]);
         this.server.addObserver(args[0], player);
 
+        try {
+            this.player.update("1");
+        } catch (Exception e) {
+            System.out.println("Success code couldn't be sent; server error");
+        }
         return 1;
     }
 }
--- Socket Server/src/main/java/org/diseno_de_softwareIC/Main.java
+++ Socket Server/src/main/java/org/diseno_de_softwareIC/Main.java
@@ -1,15 +1,38 @@
 package org.diseno_de_softwareIC;
 
 import Classes.ServerClasses.Server;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Server.getInstance();
+//        Server.getInstance();
 //
-//        String[] test = "command player1 player2".split(" ");
-//        System.out.println(Arrays.toString(test));
+        String test = "[{\"name\":\"michael myers\",\"type\":\"FIRE\",\"image\":\"<path to image>\",\"weapons\":[\"knife\",\"gun\"]},{\"name\":\"michael myers\",\"type\":\"FIRE\",\"image\":\"<path to image>\",\"weapons\":[\"knife\",\"gun\"]}]";
+        JSONArray jsonObject = new JSONArray(test);
+
+        // loop through JSONArray
+        for(int i = 0; i < jsonObject.length(); i++){
+            JSONObject jsonobject = jsonObject.getJSONObject(i);
+            String name = jsonobject.getString("name");
+            String type = jsonobject.getString("type");
+            String image = jsonobject.getString("image");
+            JSONArray weapons = jsonobject.getJSONArray("weapons");
+
+            // loop array
+            String[] weaponArray = new String[weapons.length()];
+            for(int j = 0; j < weapons.length(); j++){
+                weaponArray[j] = weapons.getString(j);
+            }
+
+            System.out.println("name: " + name);
+            System.out.println("type: " + type);
+            System.out.println("image: " + image);
+            System.out.println("weapons: " + Arrays.toString(weaponArray));
+        }
+
 
 
     }
