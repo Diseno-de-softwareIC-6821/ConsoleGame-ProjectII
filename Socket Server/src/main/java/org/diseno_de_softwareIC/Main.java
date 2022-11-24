84d3b645b8c80adc41c290522ac80f8a880b29da
Corne1401 <47864642+danielcornejo14@users.noreply.github.com>
2022-11-24 00:22:59 -0600

    GameWeapon double->Double

--- Socket Server/src/main/java/org/diseno_de_softwareIC/Main.java
+++ Socket Server/src/main/java/org/diseno_de_softwareIC/Main.java
@@ -5,33 +5,40 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 //        Server.getInstance();
 //
-        String test = "[{\"name\":\"michael myers\",\"type\":\"FIRE\",\"image\":\"<path to image>\",\"weapons\":[\"knife\",\"gun\"]},{\"name\":\"michael myers\",\"type\":\"FIRE\",\"image\":\"<path to image>\",\"weapons\":[\"knife\",\"gun\"]}]";
-        JSONArray jsonObject = new JSONArray(test);
+//        String test = "[{\"name\":\"michael myers\",\"type\":\"FIRE\",\"image\":\"<path to image>\",\"weapons\":[\"knife\",\"gun\"]},{\"name\":\"michael myers\",\"type\":\"FIRE\",\"image\":\"<path to image>\",\"weapons\":[\"knife\",\"gun\"]}]";
+//        JSONArray jsonObject = new JSONArray(test);
+//
+//        // loop through JSONArray
+//        for(int i = 0; i < jsonObject.length(); i++){
+//            JSONObject jsonobject = jsonObject.getJSONObject(i);
+//            String name = jsonobject.getString("name");
+//            String type = jsonobject.getString("type");
+//            String image = jsonobject.getString("image");
+//            JSONArray weapons = jsonobject.getJSONArray("weapons");
+//
+//            // loop array
+//            String[] weaponArray = new String[weapons.length()];
+//            for(int j = 0; j < weapons.length(); j++){
+//                weaponArray[j] = weapons.getString(j);
+//            }
+//
+//            System.out.println("name: " + name);
+//            System.out.println("type: " + type);
+//            System.out.println("image: " + image);
+//            System.out.println("weapons: " + Arrays.toString(weaponArray));
+//        }
 
-        // loop through JSONArray
-        for(int i = 0; i < jsonObject.length(); i++){
-            JSONObject jsonobject = jsonObject.getJSONObject(i);
-            String name = jsonobject.getString("name");
-            String type = jsonobject.getString("type");
-            String image = jsonobject.getString("image");
-            JSONArray weapons = jsonobject.getJSONArray("weapons");
+        HashMap<Integer, Double> test = new HashMap<>();
+        test.put(1, 1.0);
+        test.put(2, 2.0);
 
-            // loop array
-            String[] weaponArray = new String[weapons.length()];
-            for(int j = 0; j < weapons.length(); j++){
-                weaponArray[j] = weapons.getString(j);
-            }
-
-            System.out.println("name: " + name);
-            System.out.println("type: " + type);
-            System.out.println("image: " + image);
-            System.out.println("weapons: " + Arrays.toString(weaponArray));
-        }
+        System.out.println(test.get(1)+1);
 
 
 
